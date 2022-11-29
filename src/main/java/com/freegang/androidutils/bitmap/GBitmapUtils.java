5fbedab239a42c897b133cb4738def1829115f9f
freegang <30917584+GangJust@users.noreply.github.com>
2022-11-29 15:47:00 +0800

    Update GBitmapUtils.java

--- src/main/java/com/freegang/androidutils/bitmap/GBitmapUtils.java
+++ src/main/java/com/freegang/androidutils/bitmap/GBitmapUtils.java
@@ -163,4 +163,24 @@ public class GBitmapUtils {
 
         return outBitmap;
     }
+
+    /**
+     * 将bitmap转换为本地的图片
+     *
+     * @param bitmap
+     * @return
+     */
+    public static boolean bitmap2Path(Bitmap bitmap, String path, String filename) {
+        try {
+            OutputStream output = new FileOutputStream(new File(path, filename));
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, output);
+            output.flush();
+            output.close();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            GLogUtils.xException(e.getMessage());
+        }
+        return false;
+    }
 }
