962ce8d3393e23101e229b535aaf19606a6ec131
GangJust <201741884@qq.com>
2022-12-09 15:20:01 +0800

    增加注释信息
    增加注释信息

--- src/main/java/com/freegang/androidutils/net/GURLPramsUtils.java
+++ src/main/java/com/freegang/androidutils/net/GURLPramsUtils.java
@@ -4,6 +4,7 @@ import java.net.URLEncoder;
 import java.util.Map;
 
 public class GURLPramsUtils {
+	
     private GURLPramsUtils() {
         ///
     }
@@ -11,8 +12,8 @@ public class GURLPramsUtils {
     /**
      * URL拼接, 通常用于GET请求
      *
-     * @param baseUrl
-     * @param map
+     * @param baseUrl url
+     * @param map     参数列表
      * @return
      */
     public static String urlSplicing(String baseUrl, Map<String, String> map) {
@@ -22,8 +23,9 @@ public class GURLPramsUtils {
     /**
      * URL拼接, 通常用于GET请求
      *
-     * @param baseUrl
-     * @param map
+     * @param baseUrl      url
+     * @param map          参数列表
+     * @param needEncoding 是否需要URL编码
      * @return
      */
     public static String urlSplicing(String baseUrl, Map<String, String> map, boolean needEncoding) {
@@ -34,6 +36,7 @@ public class GURLPramsUtils {
     /**
      * Url参数编码, 通常用于GET请求
      *
+     * @param map 参数列表
      * @return
      */
     public static String encoding(Map<String, String> map) {
@@ -48,7 +51,7 @@ public class GURLPramsUtils {
     /**
      * 将Map转换为URL参数类型, 需要注意的是该参数作为原始参数, 并未 encoding
      *
-     * @param map
+     * @param map 参数列表
      * @return
      */
     public static String original(Map<String, String> map) {
